18aac71e10928711338cf55c6ae6020a26774e26
Sobadfish <3098143605>
2022-10-15 16:34:13 +0800

    增加接口描述

--- src/main/java/org/sobadfish/gamedemo/room/event/IEventDurationTime.java
+++ src/main/java/org/sobadfish/gamedemo/room/event/IEventDurationTime.java
@@ -1,15 +1,24 @@
 package org.sobadfish.gamedemo.room.event;
 
+/**
+ * 定时事件
+ * 每秒执行一次可终止
+ *
+ * @author Sobadfish
+ * */
 public interface IEventDurationTime {
 
 
     /**
-     * 是否超时
+     * 获取当前事件的持续事件是否结束
+     * 当返回值为false时事件 结束
+     * @return 是否超时
      * */
     boolean isOutTime();
 
     /**
      * 每秒更新一次
+     * 当未结束时，每秒执行一次update方法
      * */
     void update();
 
