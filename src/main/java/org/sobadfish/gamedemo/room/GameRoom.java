22742511ea7833840dfee993fba96cab1aa1d37d
sobadfish <3098143605@qq.com>
2023-02-02 00:41:58 +0800

    增加伤害浮空字 新增感染模式

--- src/main/java/org/sobadfish/gamedemo/room/GameRoom.java
+++ src/main/java/org/sobadfish/gamedemo/room/GameRoom.java
@@ -404,7 +404,7 @@ public class GameRoom {
                         || (s < t )){
                     if(manager.getTeamConfig().getTeamConfig().maxPlayer > 0) {
                         if(t > manager.getTeamConfig().getTeamConfig().maxPlayer) {
-                            t += (t - manager.getTeamConfig().getTeamConfig().maxPlayer);
+                            t += (int)Math.ceil(((t - manager.getTeamConfig().getTeamConfig().maxPlayer) / (float)s));
                         }
                         if(manager.getTeamConfig().getTeamConfig().maxPlayer > s) {
                             if (noTeam.size() > 0) {
